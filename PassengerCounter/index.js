3da12e49fc104d22be1399e240348ed800716110
normstraker <normstraker@yahoo.com>
2022-08-09 17:35:56 -0500

    080922 - final

--- PassengerCounter/index.js
+++ PassengerCounter/index.js
@@ -1,2 +1,18 @@
-/* access the html document by id(#) and sets inner text (between the " ") */
-// document.getElementById("count-el").innerText = 15;
+let count = 0;
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+
+function increment() {
+  // invoked by click
+  count += 1;
+  countEl.textContent = count;
+}
+
+function save() {
+  let countStr = count + " - ";
+  saveEl.textContent += countStr;
+  countEl.textContent = 0;
+  count = 0;
+}
+
+console.log("Let's count the people on the train!");
